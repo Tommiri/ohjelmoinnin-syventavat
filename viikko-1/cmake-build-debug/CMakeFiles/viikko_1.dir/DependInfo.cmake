c024d84b2e30b3ed90d464c48e1ef287c1cc16f3
Tommi Riiheläinen <tommi.riihelainen@gmail.com>
2023-01-20 16:51:56 +0200

    Refactored folder hierarchy

---
+++ viikko-1/cmake-build-debug/CMakeFiles/viikko_1.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tommi/School/ohjelmoinnin-syventavat/viikko-1/src/Event.cpp" "CMakeFiles/viikko_1.dir/src/Event.cpp.o" "gcc" "CMakeFiles/viikko_1.dir/src/Event.cpp.o.d"
+  "/home/tommi/School/ohjelmoinnin-syventavat/viikko-1/src/days.cpp" "CMakeFiles/viikko_1.dir/src/days.cpp.o" "gcc" "CMakeFiles/viikko_1.dir/src/days.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
